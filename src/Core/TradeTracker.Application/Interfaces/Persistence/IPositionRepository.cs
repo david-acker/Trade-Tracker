fddccf0148cf50e3e8bde4bf9e2cf6afaa200a45
davidacker3@gmail.com <davidacker3@gmail.com>
2021-03-09 14:37:04 -0500

    Added member for paged positions method.

--- src/Core/TradeTracker.Application/Interfaces/Persistence/IPositionRepository.cs
+++ src/Core/TradeTracker.Application/Interfaces/Persistence/IPositionRepository.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Threading.Tasks;
+using TradeTracker.Application.Features.Positions.Queries.GetPositions;
+using TradeTracker.Application.Models.Pagination;
 using TradeTracker.Domain.Entities;
 
 namespace TradeTracker.Application.Interfaces.Persistence
@@ -7,5 +9,7 @@ namespace TradeTracker.Application.Interfaces.Persistence
     public interface IPositionRepository : IAsyncRepository<Position>
     {
         Task<Position> GetBySymbolAsync(Guid accessKey, string symbol);
+
+        Task<PagedList<Position>> GetPagedPositionsAsync(PagedPositionsResourceParameters parameters);
     }
 }
