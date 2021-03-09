5c3b52c36bc233145fd4b01e9bf62cc70c117ac2
davidacker3@gmail.com <davidacker3@gmail.com>
2021-03-08 20:50:19 -0500

    Added HATEOAS support.

--- src/API/TradeTracker.Api/Controllers/PositionsController.cs
+++ src/API/TradeTracker.Api/Controllers/PositionsController.cs
@@ -3,15 +3,18 @@ using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using TradeTracker.Api.Helpers;
 using TradeTracker.Application.Features.Positions;
 using TradeTracker.Application.Features.Positions.Queries.GetPosition;
 using TradeTracker.Application.Features.Positions.Queries.GetPositions;
+using TradeTracker.Application.Models.Navigation;
 
 namespace TradeTracker.Api.Controllers
 {
@@ -35,16 +38,53 @@ namespace TradeTracker.Api.Controllers
         }
 
         [HttpGet(Name = "GetPositions")]
-        public async Task<ActionResult<IEnumerable<PositionForReturnDto>>> GetPositions()
+        [Produces("application/json",
+            "application/vnd.trade.hateoas+json")]
+        public async Task<ActionResult<IEnumerable<PositionForReturnDto>>> GetPositions(
+            [FromHeader(Name = "Accept")] string mediaType)
         {
             _logger.LogInformation($"PositionsController: {nameof(GetPositions)} was called.");
+
+
+            if (!MediaTypeHeaderValue.TryParse(mediaType,
+                out MediaTypeHeaderValue parsedMediaType))
+            {
+                return BadRequest();
+            }
+
 
             var query = new GetPositionsQuery()
             {
                 AccessKey = Guid.Parse(User.FindFirstValue("AccessKey"))
             };
 
-            return Ok(await _mediator.Send(query));
+            var positions = await _mediator.Send(query);
+
+            var positionsToReturn = positions.ShapeData(null);
+
+            var includeLinks = parsedMediaType.SubTypeWithoutSuffix
+                .EndsWith("hateoas", StringComparison.InvariantCultureIgnoreCase);
+
+            if (includeLinks)
+            {
+                var linkedPositionsToReturn = positionsToReturn
+                    .Select(position =>
+                    {
+                       var positionAsDictionary = position as IDictionary<string, object>;
+
+                       var positionLinks = CreateLinksForPosition(
+                            (string)positionAsDictionary["Symbol"]);
+                    
+                       positionAsDictionary.Add("links", positionLinks);
+                       return positionAsDictionary;
+                    });;
+
+                return Ok(linkedPositionsToReturn);
+            }
+            else
+            {
+                return Ok(positionsToReturn);
+            }
         }
 
         [HttpOptions(Name = "OptionsForPositions")]
@@ -58,9 +98,19 @@ namespace TradeTracker.Api.Controllers
         }
 
         [HttpGet("{symbol}", Name = "GetPosition")]
-        public async Task<ActionResult<PositionForReturnDto>> GetPosition([FromRoute] string symbol)
+        [Produces("application/json",
+            "application/vnd.trade.hateoas+json")]
+        public async Task<ActionResult<PositionForReturnDto>> GetPosition(
+            [FromRoute] string symbol,
+            [FromHeader(Name = "Accept")] string mediaType)
         {
             _logger.LogInformation($"PositionsController: {nameof(GetPosition)} was called.");
+
+            if (!MediaTypeHeaderValue.TryParse(mediaType,
+                out MediaTypeHeaderValue parsedMediaType))
+            {
+                return BadRequest();
+            }
 
             var query = new GetPositionQuery()
             {
@@ -68,7 +118,21 @@ namespace TradeTracker.Api.Controllers
                 Symbol = symbol
             };
 
-            return Ok(await _mediator.Send(query));
+            var position = await _mediator.Send(query);
+
+            var positionToReturn = position.ShapeData(null) as IDictionary<string, object>;
+
+            var includeLinks = parsedMediaType.SubTypeWithoutSuffix
+                .EndsWith("hateoas", StringComparison.InvariantCultureIgnoreCase);
+
+            if (includeLinks)
+            {
+                var links = CreateLinksForPosition(symbol);
+
+                positionToReturn.Add("links", links);
+            }
+
+            return Ok(positionToReturn);
         }
 
         [HttpOptions("{symbol}", Name = "OptionsForPositionBySymbol")]
@@ -79,6 +143,21 @@ namespace TradeTracker.Api.Controllers
             Response.Headers.Add("Allow", "GET,OPTIONS");
             
             return NoContent();
+        }
+
+        private IEnumerable<LinkDto> CreateLinksForPosition(string symbol)
+        {
+            var links = new List<LinkDto>()
+            {
+                new LinkDto(
+                    Url.Link(
+                        "GetPosition", 
+                        new { symbol }),
+                    "self",
+                    "GET")
+            };
+                    
+            return links;
         }
     }
 }
