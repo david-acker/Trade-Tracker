4d50072c3487affbf11cb55602735e2c1e0bf5e2
davidacker3@gmail.com <davidacker3@gmail.com>
2021-03-10 11:22:30 -0500

    Added validation for either including/excluding.

--- src/Core/TradeTracker.Application/Features/Positions/Queries/GetPositions/GetPositionsQueryValidator.cs
+++ src/Core/TradeTracker.Application/Features/Positions/Queries/GetPositions/GetPositionsQueryValidator.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using FluentValidation;
 using TradeTracker.Application.Features.Transactions.Shared.Validators;
 
@@ -34,6 +35,28 @@ namespace TradeTracker.Application.Features.Positions.Queries.GetPositions
                     .Must(q => ValidExposureTypes.Contains(q))
                         .WithMessage($"The Exposure filter requires one of the valid types: {String.Join(", ", ValidExposureTypes)}.");
             });
+
+            When(q => (
+                (q.Including.Count() > 0) ||
+                (q.Excluding.Count() > 0)), () => 
+            {
+                RuleFor(q => q)
+                    .Must(q => HasEitherIncludingOrExcluding(q.Including, q.Excluding))
+                        .WithMessage("Including and Excluding parameters cannot be used together");
+            });
+
+        }
+
+        private bool HasEitherIncludingOrExcluding(List<string> including, List<string> excluding)
+        {
+            var isValid = false;
+            
+            if (including.Count() == 0 || excluding.Count() == 0)
+            {
+                isValid = true;
+            }
+
+            return isValid;
         }
     }
 }
