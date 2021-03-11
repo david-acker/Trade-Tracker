8998c8824bb0dccdb6b0c0882ff9cabe47a1bdd2
davidacker3@gmail.com <davidacker3@gmail.com>
2021-03-10 19:04:48 -0500

    Added attributes and XML comments.

--- src/API/TradeTracker.Api/Controllers/AccountController.cs
+++ src/API/TradeTracker.Api/Controllers/AccountController.cs
@@ -1,14 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Threading.Tasks;
 using TradeTracker.Application.Models.Authentication;
 using TradeTracker.Application.Interfaces.Identity;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
 
 namespace TradeTracker.Api.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+    [Produces("application/json")]
     public class AccountController : ControllerBase
     {
         private readonly IAuthenticationService _authenticationService;
@@ -22,15 +23,29 @@ namespace TradeTracker.Api.Controllers
             _logger = logger;
         }
 
+        /// <summary>
+        /// Authenticate an existing account.
+        /// </summary>
+        /// <param name="authenticationRequest">Authentication request containing account information</param>
+        /// <returns>An ActionResult of type AuthenticationResponse</returns>
+        /// <response code="422">Validation Error</response>
         [HttpPost("authenticate", Name="Authenticate")]
-        public async Task<ActionResult<AuthenticationResponse>> Authenticate(AuthenticationRequest request)
+        [Consumes("application/json")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        public async Task<ActionResult<AuthenticationResponse>> Authenticate(
+            AuthenticationRequest authenticationRequest)
         {
             _logger.LogInformation($"AccountController: {nameof(Authenticate)} was called.");
 
-            return Ok(await _authenticationService.AuthenticateAsync(request));
+            return Ok(await _authenticationService.AuthenticateAsync(authenticationRequest));
         }
 
+        /// <summary>
+        /// Options for /api/account/authenticate URI.
+        /// </summary>
         [HttpOptions("authenticate", Name="OptionsForAuthenticate")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public IActionResult OptionsForAuthenticate()
         {
             _logger.LogInformation($"AccountController: {nameof(OptionsForAuthenticate)} was called.");
@@ -40,15 +55,29 @@ namespace TradeTracker.Api.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Register a new account.
+        /// </summary>
+        /// <param name="registrationRequest">Registration request containing account information</param>
+        /// <returns>An ActionResult of type RegistrationResponse</returns>
+        /// <response code="422">Validation Error</response>
         [HttpPost("register", Name="Register")]
-        public async Task<ActionResult<RegistrationResponse>> Register(RegistrationRequest request)
+        [Consumes("application/json")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        public async Task<ActionResult<RegistrationResponse>> Register(
+            RegistrationRequest registrationRequest)
         {
             _logger.LogInformation($"AccountController: {nameof(Register)} was called.");
 
-            return Ok(await _authenticationService.RegisterAsync(request));
+            return Ok(await _authenticationService.RegisterAsync(registrationRequest));
         }
 
+        /// <summary>
+        /// Options for /api/account/register URI.
+        /// </summary>
         [HttpOptions("register", Name="OptionsForRegister")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public IActionResult OptionsForRegister()
         {
             _logger.LogInformation($"AccountController: {nameof(OptionsForRegister)} was called.");
