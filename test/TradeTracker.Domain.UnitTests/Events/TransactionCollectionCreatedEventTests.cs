ba4ebdfe45710c25c386b3feaef868dffb5e9c78
davidacker3@gmail.com <davidacker3@gmail.com>
2021-03-11 23:14:16 -0500

    Added unit tests.

---
+++ test/TradeTracker.Domain.UnitTests/Events/TransactionCollectionCreatedEventTests.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using TradeTracker.Domain.Common;
+using TradeTracker.Domain.Entities;
+using TradeTracker.Domain.Enums;
+using TradeTracker.Domain.Events;
+using Xunit;
+
+namespace TradeTracker.Domain.UnitTests.Events
+{
+    public class TransactionCollectionCreatedEventTests
+    {
+        [Fact]
+        public void Create_NewEvent_TransactionMapGroupedByUniqueSymbols()
+        {
+            // Arrange
+            IEnumerable<Transaction> transactionCollection = new List<Transaction>()
+            {
+                new Transaction()
+                {
+                    AccessKey = Guid.Parse("fc0bd83d-8bc2-49a6-a6ab-b94c99ad7b1a"),
+                    DateTime = new DateTime(2020, 1, 1),
+                    Symbol = "A",
+                    Type = TransactionType.BuyToOpen,
+                    Quantity = 5,
+                    Notional = 10,
+                    TradePrice = 2
+                },
+                new Transaction()
+                {
+                    AccessKey = Guid.Parse("fc0bd83d-8bc2-49a6-a6ab-b94c99ad7b1a"),
+                    DateTime = new DateTime(2020, 1, 2),
+                    Symbol = "B",
+                    Type = TransactionType.SellToOpen,
+                    Quantity = 2,
+                    Notional = 22,
+                    TradePrice = 11
+                },
+                new Transaction()
+                {
+                    AccessKey = Guid.Parse("fc0bd83d-8bc2-49a6-a6ab-b94c99ad7b1a"),
+                    DateTime = new DateTime(2020, 1, 3),
+                    Symbol = "A",
+                    Type = TransactionType.SellToClose,
+                    Quantity = 4,
+                    Notional = 12,
+                    TradePrice = 3
+                }
+            };
+
+            // Act
+            var domainEvent = new TransactionCollectionCreatedEvent(transactionCollection);
+
+            // Assert
+            var keys = new List<string>(domainEvent.TransactionMap.Keys);
+
+            keys.Should()
+                .HaveCount(k => k == 2);
+        }
+    }
+}
