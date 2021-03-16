7f00d4a4de3ce6dbe1202aad80a75d75a99e7859
davidacker3@gmail.com <davidacker3@gmail.com>
2021-03-16 14:38:49 -0400

    Created separate actions by media type

--- src/API/TradeTracker.Api/Controllers/PositionsController.cs
+++ src/API/TradeTracker.Api/Controllers/PositionsController.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using TradeTracker.Api.ActionConstraints;
 using TradeTracker.Api.Helpers;
 using TradeTracker.Application.Features.Positions.Queries.GetPosition;
 using TradeTracker.Application.Features.Positions.Queries.GetPositions;
@@ -39,6 +40,7 @@ namespace TradeTracker.Api.Controllers
             _mediator = mediator;
         }
 
+
         /// <summary>
         /// Get a paged list of positions.
         /// </summary>
@@ -61,21 +63,17 @@ namespace TradeTracker.Api.Controllers
         /// <response code="200">Returns any paged positions matching the parameters</response>
         /// <response code="422">Validation Error</response>
         [HttpGet(Name = "GetPositions")]
-        [Produces("application/json",
-            "application/vnd.trade.hateoas+json")]
+        [Consumes("application/json")]
+        [Produces("application/json", "application/json")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        [RequestHeaderMatchesMediaType("Content-Type", "application/json")]
+        [RequestHeaderMatchesMediaType("Accept", "application/json")]
+        [ApiExplorerSettings(IgnoreApi = true)]
         public async Task<IActionResult> GetPositions(
-            [FromQuery] GetPositionsResourceParameters parameters,
-            [FromHeader(Name = "Accept")] string mediaType)
+            [FromQuery] GetPositionsResourceParameters parameters)
         {
             _logger.LogInformation($"PositionsController: {nameof(GetPositions)} was called.");
-
-            if (!MediaTypeHeaderValue.TryParse(mediaType,
-                out MediaTypeHeaderValue parsedMediaType))
-            {
-                return BadRequest();
-            }
 
             var query = _mapper.Map<GetPositionsQuery>(parameters);
             query.AccessKey = Guid.Parse(User.FindFirstValue("AccessKey"));
@@ -84,55 +82,90 @@ namespace TradeTracker.Api.Controllers
 
             var positionsToReturn = pagedPositions.Items.ShapeData(null);
 
-            var includeLinks = parsedMediaType.SubTypeWithoutSuffix
-                .EndsWith("hateoas", StringComparison.InvariantCultureIgnoreCase);
+            Response.Headers.Add("X-Paging-PageNumber", pagedPositions.CurrentPage.ToString());
+            Response.Headers.Add("X-Paging-PageSize", pagedPositions.PageSize.ToString());
+            Response.Headers.Add("X-Paging-PageCount", pagedPositions.TotalPages.ToString());
+            Response.Headers.Add("X-Paging-TotalRecordCount", pagedPositions.TotalCount.ToString());
 
-            if (includeLinks)
-            {
-                var links = CreateLinksForPositions(
-                    parameters,
-                    pagedPositions.HasNext,
-                    pagedPositions.HasPrevious);
-
-                var linkedPositionsToReturn = positionsToReturn
-                    .Select(position =>
-                    {
-                       var positionAsDictionary = position as IDictionary<string, object>;
-
-                       var positionLinks = CreateLinksForPosition(
-                            (string)positionAsDictionary["Symbol"]);
-                    
-                       positionAsDictionary.Add("links", positionLinks);
-                       return positionAsDictionary;
-                    });;
-
-                var paginationMetadata = new
-                {
-                    pageNumber = pagedPositions.CurrentPage,
-                    pageSize = pagedPositions.PageSize,
-                    pageCount = pagedPositions.TotalPages,
-                    totalRecordCount = pagedPositions.TotalCount,
-                };
-
-                var linkedPositionsResource = new
-                {
-                    paginationMetadata = paginationMetadata,
-                    results = linkedPositionsToReturn,
-                    links
-                };
-
-                return Ok(linkedPositionsResource);
-            }
-            else
-            {
-                Response.Headers.Add("X-Paging-PageNumber", pagedPositions.CurrentPage.ToString());
-                Response.Headers.Add("X-Paging-PageSize", pagedPositions.PageSize.ToString());
-                Response.Headers.Add("X-Paging-PageCount", pagedPositions.TotalPages.ToString());
-                Response.Headers.Add("X-Paging-TotalRecordCount", pagedPositions.TotalCount.ToString());
-
-                return Ok(positionsToReturn);
-            }
+            return Ok(positionsToReturn);
         }
+        
+
+        /// <summary>
+        /// Get a paged list of positions.
+        /// </summary>
+        /// <param name="parameters">The resource parameters for specifying the returned positions</param>
+        /// <remarks>
+        /// Example: \
+        /// GET /api/positions \
+        /// { \
+        ///     "orderBy": "Quantity", \
+        ///     "pageNumber": "3" \
+        ///     "pageSize": "25", \
+        ///     "excluding": [ \
+        ///         "QRS", \
+        ///         "TUV", \
+        ///         "WXY" \
+        ///     ], \
+        ///     "Exposure": "Long", \
+        /// } 
+        /// </remarks>
+        /// <response code="200">Returns any paged positions matching the parameters</response>
+        /// <response code="422">Validation Error</response>
+        [HttpGet(Name = "GetPositions")]
+        [Consumes("application/json")]
+        [Produces("application/json", "application/vnd.trade.hateoas+json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        [RequestHeaderMatchesMediaType("Content-Type", "application/json")]
+        [RequestHeaderMatchesMediaType("Accept", "application/vnd.trade.hateoas+json")]
+        public async Task<IActionResult> GetPositionsWithLinks(
+            [FromQuery] GetPositionsResourceParameters parameters)
+        {
+            _logger.LogInformation($"PositionsController: {nameof(GetPositionsWithLinks)} was called.");
+
+            var query = _mapper.Map<GetPositionsQuery>(parameters);
+            query.AccessKey = Guid.Parse(User.FindFirstValue("AccessKey"));
+
+            var pagedPositions = await _mediator.Send(query);
+
+            var positionsToReturn = pagedPositions.Items.ShapeData(null);
+
+            var links = CreateLinksForPositions(
+                parameters,
+                pagedPositions.HasNext,
+                pagedPositions.HasPrevious);
+
+            var linkedPositionsToReturn = positionsToReturn
+                .Select(position =>
+                {
+                    var positionAsDictionary = position as IDictionary<string, object>;
+
+                    var positionLinks = CreateLinksForPosition(
+                        (string)positionAsDictionary["Symbol"]);
+                
+                    positionAsDictionary.Add("links", positionLinks);
+                    return positionAsDictionary;
+                });;
+
+            var paginationMetadata = new
+            {
+                pageNumber = pagedPositions.CurrentPage,
+                pageSize = pagedPositions.PageSize,
+                pageCount = pagedPositions.TotalPages,
+                totalRecordCount = pagedPositions.TotalCount,
+            };
+
+            var linkedPositionsResource = new
+            {
+                paginationMetadata = paginationMetadata,
+                results = linkedPositionsToReturn,
+                links
+            };
+
+            return Ok(linkedPositionsResource);
+        }
+
 
         /// <summary>
         /// Options for /api/positions URI.
@@ -152,6 +185,7 @@ namespace TradeTracker.Api.Controllers
             return NoContent();
         }
 
+
         /// <summary>
         /// Gets a position by symbol of its security.
         /// </summary>
@@ -163,22 +197,17 @@ namespace TradeTracker.Api.Controllers
         /// <response code="200">Returns the requested position</response>
         /// <response code="422">Validation Error</response>
         [HttpGet("{symbol}", Name = "GetPosition")]
-        [Produces("application/json",
-            "application/vnd.trade.hateoas+json")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        [RequestHeaderMatchesMediaType("Content-Type", "application/json")]
+        [RequestHeaderMatchesMediaType("Accept", "application/json")]
         public async Task<IActionResult> GetPosition(
-            [FromRoute] string symbol,
-            [FromHeader(Name = "Accept")] string mediaType)
+            [FromRoute] string symbol)
         {
             _logger.LogInformation($"PositionsController: {nameof(GetPosition)} was called.");
-
-            if (!MediaTypeHeaderValue.TryParse(mediaType,
-                out MediaTypeHeaderValue parsedMediaType))
-            {
-                return BadRequest();
-            }
 
             var query = new GetPositionQuery()
             {
@@ -190,18 +219,50 @@ namespace TradeTracker.Api.Controllers
 
             var positionToReturn = position.ShapeData(null) as IDictionary<string, object>;
 
-            var includeLinks = parsedMediaType.SubTypeWithoutSuffix
-                .EndsWith("hateoas", StringComparison.InvariantCultureIgnoreCase);
+            return Ok(positionToReturn);
+        }
 
-            if (includeLinks)
+
+        /// <summary>
+        /// Gets a position by symbol of its security.
+        /// </summary>
+        /// <param name="symbol">The symbol for the position</param>
+        /// <remarks>
+        /// Example: \
+        /// GET /api/positions/{symbol} 
+        /// </remarks>
+        /// <response code="200">Returns the requested position</response>
+        /// <response code="422">Validation Error</response>
+        [HttpGet("{symbol}", Name = "GetPosition")]
+        [Consumes("application/json")]
+        [Produces("application/vnd.trade.hateoas+json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        [RequestHeaderMatchesMediaType("Content-Type", "application/json")]
+        [RequestHeaderMatchesMediaType("Accept", "application/vnd.trade.hateoas+json")]
+        public async Task<IActionResult> GetPositionWithLinks(
+            [FromRoute] string symbol)
+        {
+            _logger.LogInformation($"PositionsController: {nameof(GetPosition)} was called.");
+
+            var query = new GetPositionQuery()
             {
-                var links = CreateLinksForPosition(symbol);
+                AccessKey = Guid.Parse(User.FindFirstValue("AccessKey")),
+                Symbol = symbol
+            };
 
-                positionToReturn.Add("links", links);
-            }
+            var position = await _mediator.Send(query);
+
+            var positionToReturn = position.ShapeData(null) as IDictionary<string, object>;
+
+            var links = CreateLinksForPosition(symbol);
+
+            positionToReturn.Add("links", links);
 
             return Ok(positionToReturn);
         }
+
 
         /// <summary>
         /// Options for /api/positions/{symbol} URI.
@@ -221,6 +282,7 @@ namespace TradeTracker.Api.Controllers
             return NoContent();
         }
 
+
         private IEnumerable<LinkDto> CreateLinksForPosition(string symbol)
         {
             var links = new List<LinkDto>()
@@ -235,6 +297,7 @@ namespace TradeTracker.Api.Controllers
                     
             return links;
         }
+
 
         private IEnumerable<LinkDto> CreateLinksForPositions(
             GetPositionsResourceParameters parameters,
@@ -275,6 +338,7 @@ namespace TradeTracker.Api.Controllers
 
             return links;
         }
+
 
         private string CreatePositionsResourceUrl(
             GetPositionsResourceParameters parameters,
