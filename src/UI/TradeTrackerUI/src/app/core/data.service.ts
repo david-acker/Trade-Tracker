482a6afc3088a59dd2319f71997be360ef61699c
davidacker3@gmail.com <davidacker3@gmail.com>
2021-03-22 12:03:05 -0400

    Removed user related functionality, created addTransaction

--- src/UI/TradeTrackerUI/src/app/core/data.service.ts
+++ src/UI/TradeTrackerUI/src/app/core/data.service.ts
@@ -1,11 +1,13 @@
 import { query } from '@angular/animations';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Credentials } from '../models/credentials';
 import { PagedTransactionsWithLinks } from '../models/paged-transactions-with-links';
+import { Registration } from '../models/registration';
 import { PagedTransactionsParameters } from '../models/resource-parameters/paged-transactions-parameters';
+import { TransactionForCreation } from '../models/transaction-for-creation';
 
 @Injectable({
     providedIn: 'root'
@@ -16,51 +18,78 @@ export class DataService {
 
     public pagedTransactionsWithLinks: PagedTransactionsWithLinks;
 
-    public get loginRequired(): boolean {
-        let token: string = JSON.parse(localStorage.getItem('token'));
-        let tokenExpiration: Date = new Date(JSON.parse(localStorage.getItem('token-expiration')));
+    // public get loginRequired(): boolean {
+    //     let token: string = JSON.parse(localStorage.getItem('token'));
+    //     let tokenExpiration: Date = new Date(JSON.parse(localStorage.getItem('token-expiration')));
         
-        return token.length == 0 || tokenExpiration > new Date();
-    }
+    //     return token.length == 0 || tokenExpiration > new Date();
+    // }
 
     public login(credentials: Credentials) {
-        return this.http.post("/api/account/authenticate", credentials)
+        return this.http.post('/api/account/authenticate', credentials)
             .pipe(
                 map((response: any) => {
                     localStorage.setItem('token', response.token);
-                    localStorage.setItem('token-expiration', response.expiration);
+
+                    let expirationDate = new Date(response.expiration);
+                    localStorage.setItem('token-expiration', expirationDate.toISOString());
 
                     return true;
                 }));
+    }
+
+    public register(registration: Registration) {
+        return this.http.post('/api/account/register', registration)
+            .pipe(
+                map((response: any) => {
+                    return true;
+                }));
+    }
+
+    public addTransaction(transaction: TransactionForCreation) {
+
+        let token: string = localStorage.getItem('token');
+
+        let headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${token}`
+        });
+
+        return this.http.post('api/transactions', transaction, {
+            headers: headers
+        }).pipe(
+            map((response: any) => {
+                return true;
+            }));
     }
 
     public getPagedTransactionsWithLinks(resourceParams: PagedTransactionsParameters): Observable<boolean> {
 
         let token: string = localStorage.getItem('token');
 
-        const requestHeaders = {
+        let headers = new HttpHeaders({
             'Content-Type': 'application/json',
             'Accept': 'application/vnd.trade.hateoas+json',
             'Authorization': `Bearer ${token}`
-        }
+        });
 
-        let queryParams = new HttpParams()
+        let params = new HttpParams()
 
-        queryParams = queryParams.set('Order', resourceParams.order);
-        queryParams = queryParams.set('PageNumber', resourceParams.pageNumber.toString());
-        queryParams = queryParams.set('PageSize', resourceParams.pageSize.toString());
+        params = params.set('Order', resourceParams.order);
+        params = params.set('PageNumber', resourceParams.pageNumber.toString());
+        params = params.set('PageSize', resourceParams.pageSize.toString());
         if (resourceParams.rangeStart != null) {
-            queryParams = queryParams.set('RangeStart', resourceParams.rangeStart.toString());
+            params = params.set('RangeStart', resourceParams.rangeStart.toString());
         }
         if (resourceParams.rangeEnd != null) {
-            queryParams = queryParams.set('RangeEnd', resourceParams.rangeEnd.toString());
+            params = params.set('RangeEnd', resourceParams.rangeEnd.toString());
         }
         
         return this.http.get('/api/transactions', {
-            headers: requestHeaders,
-            params: queryParams
-        })
-        .pipe(
+            headers: headers,
+            params: params
+        }).pipe(
             map((data: any) => {
                 this.pagedTransactionsWithLinks = data;
                 return true;
