c330a28e89dca349bddff14d93a9a631f343e032
davidacker3@gmail.com <davidacker3@gmail.com>
2021-03-22 12:03:52 -0400

    Created dto

---
+++ src/UI/TradeTrackerUI/src/app/models/registration.ts
@@ -0,0 +1,7 @@
+export class Registration {
+    public firstName: string = '';
+    public lastName: string = '';
+    public email: string = '';
+    public userName: string = '';
+    public password: string = '';
+}
