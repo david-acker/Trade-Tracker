c98316a0b4232d4023d93dbdc0c9f06b019adcfd
davidacker3@gmail.com <davidacker3@gmail.com>
2021-03-22 12:03:30 -0400

    Created dto

---
+++ src/UI/TradeTrackerUI/src/app/models/transaction-for-creation.ts
@@ -0,0 +1,8 @@
+export class TransactionForCreation {
+    public dateTime: string;
+    public symbol: string;
+    public type: string;
+    public quantity: number;
+    public notional: number;
+    public tradePrice: number;
+}
