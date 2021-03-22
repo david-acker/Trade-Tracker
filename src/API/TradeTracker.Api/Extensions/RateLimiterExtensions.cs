b676d204f6f4d76528d90490929326f789a426fd
davidacker3@gmail.com <davidacker3@gmail.com>
2021-03-22 11:59:00 -0400

    Created RateLimiterExtensions

---
+++ src/API/TradeTracker.Api/Extensions/RateLimiterExtensions.cs
@@ -0,0 +1,20 @@
+using AspNetCoreRateLimit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TradeTracker.Api.Extensions
+{
+    public static class RateLimiterExtensions
+    {
+        public static void AddRateLimiter(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMemoryCache();
+            services.Configure<IpRateLimitOptions>(
+                configuration.GetSection("IpRateLimiting"));
+            services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+            services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+            services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+            services.AddHttpContextAccessor();
+        }
+    }
+}
