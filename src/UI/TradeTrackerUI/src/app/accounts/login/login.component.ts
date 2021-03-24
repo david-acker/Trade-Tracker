c61d5af4f05e28f6d7a1ea2b496c3223a1a4cc9c
DavidDAcker <davidacker3@gmail.com>
2021-03-24 13:57:58 -0400

    Converted to ReactiveForms

--- src/UI/TradeTrackerUI/src/app/accounts/login/login.component.ts
+++ src/UI/TradeTrackerUI/src/app/accounts/login/login.component.ts
@@ -1,28 +1,46 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
-import { DataService } from '../../core/data.service';
+import { AuthService } from 'src/app/core/auth.service';
 import { Credentials } from '../../models/accounts/credentials';
 
 @Component({
     templateUrl: 'login.component.html',
     styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-    
-    constructor(private data: DataService, private router: Router) { }
+export class LoginComponent implements OnInit {
+    private errorMessage: string = '';
+    private returnUrl: string;
 
-    public credentials: Credentials = new Credentials();
+    public loginForm: FormGroup;
 
-    errorMessage: string = "";
+    constructor(
+        private authService: AuthService,
+        private fb: FormBuilder,
+        private route: ActivatedRoute, 
+        private router: Router) { }
+
+    ngOnInit(): void {
+        this.authService.clearStoredToken();
+
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '';
+
+        this.loginForm = this.fb.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required]]
+        });
+    }
 
     onLogin() {
-        this.errorMessage = "";
-        this.data.login(this.credentials)
+        let credentials: Credentials = this.loginForm.value;
+        this.errorMessage = '';
+
+        this.authService.login(credentials)
             .subscribe(success => {
-                if (success) {
-                    this.router.navigate([""]);
-                }
-            }, err => this.errorMessage = "Failed to login");
+                this.router.navigateByUrl(this.returnUrl);
+            }, error => {
+                this.errorMessage = 'Failed to login'
+            });
     }
 }
