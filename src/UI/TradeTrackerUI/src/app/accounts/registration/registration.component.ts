bb9b75517fe3744765eb611d1425b3c4c3a0ba03
DavidDAcker <davidacker3@gmail.com>
2021-03-24 11:20:47 -0400

    Converted to ReactiveForms

--- src/UI/TradeTrackerUI/src/app/accounts/registration/registration.component.ts
+++ src/UI/TradeTrackerUI/src/app/accounts/registration/registration.component.ts
@@ -1,23 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from 'src/app/core/data.service';
-import { Registration } from 'src/app/models/registration';
+import { Registration } from 'src/app/models/accounts/registration';
 
 @Component({
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
 
-  constructor(private data: DataService, private router: Router) { }
-
-  public registration: Registration = new Registration();
+  public registrationForm: FormGroup;
 
   errorMessage: string = '';
-  
+
+  constructor(
+    private data: DataService,
+    private fb: FormBuilder, 
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.registrationForm = this.fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      userName: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
   onRegister() {
       this.errorMessage = '';
-      this.data.register(this.registration)
+
+      let registration: Registration = this.registrationForm.value;
+      
+      this.data.register(registration)
           .subscribe(success => {
               if (success) {
                   this.router.navigate(['Login']);
