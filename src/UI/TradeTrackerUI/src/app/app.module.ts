da6d8ae88f45bff4ae5e60454f882a65b089db08
DavidDAcker <davidacker3@gmail.com>
2021-03-24 13:57:35 -0400

    Updated providers

--- src/UI/TradeTrackerUI/src/app/app.module.ts
+++ src/UI/TradeTrackerUI/src/app/app.module.ts
@@ -1,19 +1,21 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DataService } from './core/data.service';
-import { TransactionsModule } from './transactions/transactions.module';
 import { AccountsModule } from './accounts/accounts.module';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthGuard } from './core/auth-guard.service';
+import { AuthService } from './core/auth.service';
+import { DataService } from './core/data.service';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { NgbdDatepickerPopupModule } from './datepicker/datepicker-popup.module';
 import { PositionsModule } from './positions/positions.module';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { UserService } from './core/user.service';
+import { TransactionsModule } from './transactions/transactions.module';
+
 
 @NgModule({
   declarations: [
@@ -33,8 +35,9 @@ import { UserService } from './core/user.service';
     PositionsModule,
   ],
   providers: [
+    AuthGuard,
     DataService,
-    UserService
+    AuthService
   ],
   bootstrap: [AppComponent]
 })
