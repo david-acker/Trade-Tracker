8adc55a327ada02ff6c890601a7c0645bc2a2a78
DavidDAcker <davidacker3@gmail.com>
2021-03-24 22:42:47 -0400

    Organization

--- src/UI/TradeTrackerUI/src/app/app.module.ts
+++ src/UI/TradeTrackerUI/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AccountsModule } from './accounts/accounts.module';
 import { AppComponent } from './app.component';
@@ -11,11 +12,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AuthGuard } from './core/auth-guard.service';
 import { AuthService } from './core/auth.service';
 import { DataService } from './core/data.service';
+import { DisplayTableModule } from './display-table/display-table.module';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { NgbdDatepickerPopupModule } from './datepicker/datepicker-popup.module';
 import { PositionsModule } from './positions/positions.module';
 import { TransactionsModule } from './transactions/transactions.module';
-
 
 @NgModule({
   declarations: [
@@ -23,21 +24,23 @@ import { TransactionsModule } from './transactions/transactions.module';
     NavMenuComponent
   ],
   imports: [
-    AppRoutingModule,
     AccountsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
+    DisplayTableModule,
     FontAwesomeModule,
     FormsModule,
     HttpClientModule,
-    TransactionsModule,
-    BrowserAnimationsModule,
+    NgbModule,
     NgbdDatepickerPopupModule,
     PositionsModule,
+    TransactionsModule,
   ],
   providers: [
     AuthGuard,
-    DataService,
-    AuthService
+    AuthService,
+    DataService
   ],
   bootstrap: [AppComponent]
 })
