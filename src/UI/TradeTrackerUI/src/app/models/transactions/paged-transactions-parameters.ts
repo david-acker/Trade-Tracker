dc281913f65f8ac310ff090fa35e61fd57281507
DavidDAcker <davidacker3@gmail.com>
2021-03-24 22:46:14 -0400

    Added property for type

--- src/UI/TradeTrackerUI/src/app/models/transactions/paged-transactions-parameters.ts
+++ src/UI/TradeTrackerUI/src/app/models/transactions/paged-transactions-parameters.ts
@@ -1,5 +1,6 @@
 export class PagedTransactionsParameters {
     public order: string = 'DateTime+desc';
+    public type: string = '';
     public pageNumber: number = 1;
     public pageSize: number = 10;
     public rangeStart?: string;
