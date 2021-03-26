52474508451ae7a896de1165f14902c180689b02
DavidDAcker <davidacker3@gmail.com>
2021-03-25 21:34:12 -0400

    Implemented Including and Excluding, validation overhaul, styling

--- src/UI/TradeTrackerUI/src/app/transactions/transactions-list/transaction-list.component.ts
+++ src/UI/TradeTrackerUI/src/app/transactions/transactions-list/transaction-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, AbstractControl, ValidatorFn, FormArray } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidatorFn, FormArray, FormControl } from '@angular/forms';
 
 import { DataService } from '../../core/data.service';
 import { PagedTransactionsParameters } from '../../models/transactions/paged-transactions-parameters';
@@ -22,7 +22,9 @@ export class TransactionListComponent implements OnInit {
 
     pageNumber: number = 1;
     pageSize: number = 10;
+    pageSizes: number[] = [ 10, 25, 50, 100 ];
     pageCount: number;
+    totalRecordCount: number;
 
     sortDirection: string = 'desc';
     sortDirectionName: string = 'Descending';
@@ -33,7 +35,7 @@ export class TransactionListComponent implements OnInit {
 
     constructor (
         private data: DataService, 
-        private fb: FormBuilder) { 
+        private fb: FormBuilder) {
         this.getPageData();
     }
 
@@ -42,14 +44,47 @@ export class TransactionListComponent implements OnInit {
             type: 'Any',
             orderBy: 'DateTime',
             dateRange: this.fb.group({
-                start: [''],
-                end: ['']
-            }, { validator: dateRangeChecker })
+                start: ['', [isValidDate]],
+                end: ['', [isValidDate]]
+            }, { validators: dateRangeValidator }),
+            selection: this.fb.group({
+                including: ['', [commaSeparatedValidator]],
+                excluding: ['', [commaSeparatedValidator]]
+            })
         });
     }
 
+    public isFieldEmpty(field: string): boolean {
+        const fieldValue = this.transactionFilters.get(['selection', field]).value;
+        
+        return fieldValue == '' || fieldValue == null;
+    }
+
+    public onSelectionChange(updatedFieldName: string): void {
+        let correspondingFieldName: string;
+
+        if (updatedFieldName == 'including') {
+            correspondingFieldName = 'excluding';
+        } else {
+            correspondingFieldName = 'including';
+        }
+
+        let correspondingControl: AbstractControl = 
+            this.transactionFilters.get(['selection', correspondingFieldName]);
+
+        if (!this.isFieldEmpty(updatedFieldName)) {
+            if (!correspondingControl.disabled) {
+                correspondingControl.disable();
+            }
+        } else {
+            if (correspondingControl.disabled) {
+                correspondingControl.enable();
+            }
+        }
+    }
+
     public onUpdate(): void {
-        let type: string = this.transactionFilters.get('type').value;
+        const type: string = this.transactionFilters.get('type').value;
 
         if (type == 'Any') {
             this.resourceParameters.type = '';
@@ -57,14 +92,41 @@ export class TransactionListComponent implements OnInit {
             this.resourceParameters.type = type;
         }
 
-        let orderBy: string = this.transactionFilters.get('orderBy').value;
+        const orderBy: string = 
+            this.transactionFilters.get('orderBy').value;
         this.resourceParameters.order = orderBy + '+' + this.sortDirection;
 
-        this.resourceParameters.rangeStart = parseDateInput(
-            this.transactionFilters.get(['dateRange', 'start']).value);
+        const startDateString: string = 
+            this.transactionFilters.get(['dateRange', 'start']).value;
+        if (startDateString != null && startDateString != '') {
+            this.resourceParameters.rangeStart = 
+                parseDateInputToISO(startDateString);
+        } else {
+            this.resourceParameters.rangeStart = null;
+        }
+
+        const endDateString: string = 
+            this.transactionFilters.get(['dateRange', 'end']).value;
+        if (endDateString != null && endDateString != '') {
+            this.resourceParameters.rangeEnd = 
+                parseDateInputToISO(endDateString);
+        } else {
+            this.resourceParameters.rangeEnd = null;
+        }
         
-        this.resourceParameters.rangeEnd = parseDateInput(
-            this.transactionFilters.get(['dateRange', 'end']).value);
+        if (!this.isFieldEmpty('including')) {
+            this.resourceParameters.including = 
+                this.transactionFilters.get(['selection', 'including']).value;
+        } else {
+            this.resourceParameters.including = null;
+        }
+
+        if (!this.isFieldEmpty('excluding')) {
+            this.resourceParameters.excluding = 
+                this.transactionFilters.get(['selection', 'excluding']).value;
+        } else {
+            this.resourceParameters.excluding = null;
+        }
 
         console.log(this.resourceParameters);
 
@@ -73,6 +135,12 @@ export class TransactionListComponent implements OnInit {
 
     public onPageChange(pageNumber: number): void {
         this.resourceParameters.pageNumber = pageNumber;
+        this.getPageData();
+    }
+
+    public onPageSizeChange(event: any): void {
+        let pageSize = event.target.value;
+        this.resourceParameters.pageSize = pageSize;
         this.getPageData();
     }
 
@@ -97,13 +165,14 @@ export class TransactionListComponent implements OnInit {
                 this.pageNumber = this.response.metadata.pageNumber;
                 this.pageSize = this.response.metadata.pageSize;
                 this.pageCount = this.response.metadata.pageCount;
+                this.totalRecordCount = this.response.metadata.totalRecordCount;
             }, error => {
                 this.errorMessage = 'An error occurred while loading the transactions'
             });
     }
 }
 
-function dateRangeChecker(c: AbstractControl): { [key: string]: boolean } | null {
+function dateRangeValidator(c: AbstractControl): { [key: string]: boolean } | null {
     const startControl = c.get('start');
     const endControl = c.get('end');
 
@@ -111,24 +180,81 @@ function dateRangeChecker(c: AbstractControl): { [key: string]: boolean } | null
         return null;
     }
 
-    let startDate = new Date(startControl.value);
-    let endDate = new Date(endControl.value);
+    const startDateString = startControl.value;
+    const endDateString = endControl.value;
 
-    if (startDate > endDate) {
+    if ((startDateString == '' || startDateString == null) &&
+        (endDateString == '' || endDateString == null)) {
         return null;
     }
 
-    return { startBeforeEnd: true };
+    const isValidStartDate = isValidDate(startDateString);
+    const isValidEndDate = isValidDate(endDateString);
+
+    if (!isValidStartDate && !isValidEndDate) {
+        return null;
+    }
+
+    const startDate = new Date(parseDateInput(startDateString));
+    const endDate = new Date(parseDateInput(endDateString));
+    
+    if (startDate < endDate) {
+        return null;
+    }
+
+    return { dateRangeValidator: true };
 }
 
-function parseDateInput(input: any): string | null {
+function isValidDate(input: any): boolean | null {
     if (input !== null && input !== '') {
-        let date = new Date(
-            input.year, 
-            input.month, 
-            input.day);
-        return date.toISOString();
+        const year = Number(input.year);
+        const month = Number(input.month);
+        const day = Number(input.day);
+
+        if (isNaN(year) || isNaN(month) || isNaN(day)) {
+            return false;
+        }
+
+        const date = new Date(year, month - 1, day);
+
+        return date instanceof Date && !isNaN(date.valueOf());
+    }
+
+    return true;
+}
+
+function commaSeparatedValidator(control: AbstractControl): {[key: string]: boolean} | null {
+    const input: string = control.value;
+    if (input != null && input != '') {
+        let splitString: string[] = (input.split(','));
+
+        const regex = /[^a-zA-Z\d\s:]/;
+
+        let isValid = true;
+        splitString.forEach(element => {
+            if (element.match(regex) != null) {
+                isValid = false;
+            }
+        });
+
+        return !isValid 
+            ? { commaSeparatedValidator: true } 
+            : null;
     }
 
     return null;
+}
+
+function parseDateInput(input: any): Date {
+    const year = Number(input.year);
+    const month = Number(input.month);
+    const day = Number(input.day);
+
+    return new Date(year, month - 1, day);
+}
+
+function parseDateInputToISO(input: any): string {
+    const date = new Date(parseDateInput(input));
+
+    return date.toISOString();
 }
