d45078c519614d7cd94e39712d1be5fbda9fe01c
DavidDAcker <davidacker3@gmail.com>
2021-03-31 17:09:06 -0400

    Add LoggedInUserService

--- src/Core/TradeTracker.Application/Features/Transactions/Commands/DeleteTransaction/DeleteTransactionCommandHandler.cs
+++ src/Core/TradeTracker.Application/Features/Transactions/Commands/DeleteTransaction/DeleteTransactionCommandHandler.cs
@@ -1,8 +1,10 @@
 using AutoMapper;
 using MediatR;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using TradeTracker.Application.Exceptions;
+using TradeTracker.Application.Interfaces;
 using TradeTracker.Application.Interfaces.Persistence;
 using TradeTracker.Application.Requests;
 using TradeTracker.Domain.Entities;
@@ -11,24 +13,33 @@ using TradeTracker.Domain.Events;
 
 namespace TradeTracker.Application.Features.Transactions.Commands.DeleteTransaction
 {
-    public class DeleteTransactionCommandHandler : 
-        ValidatableRequestHandler<DeleteTransactionCommand>,
+    public class DeleteTransactionCommandHandler :
         IRequestHandler<DeleteTransactionCommand>
     {
+        private readonly ILoggedInUserService _loggedInUserService;
         private readonly ITransactionRepository _transactionRepository;
         private readonly IMapper _mapper;
         
-        public DeleteTransactionCommandHandler(IMapper mapper, ITransactionRepository transactionRepository)
+        public DeleteTransactionCommandHandler(
+            ILoggedInUserService loggedInUserService,
+            IMapper mapper, 
+            ITransactionRepository transactionRepository)
         {
+            _loggedInUserService = loggedInUserService;
             _mapper = mapper;
             _transactionRepository = transactionRepository;
         }
 
         public async Task<Unit> Handle(DeleteTransactionCommand request, CancellationToken cancellationToken)
         {
-            await ValidateRequest(request);
+            Guid userAccessKey = _loggedInUserService.AccessKey;
+            
+            if (userAccessKey == Guid.Empty)
+            {
+                throw new ValidationException("The current session has expired. Please reload and log back in.");
+            }
 
-            var transaction = await _transactionRepository.GetByIdAsync(request.AccessKey, request.TransactionId);
+            var transaction = await _transactionRepository.GetByIdAsync(userAccessKey, request.TransactionId);
 
             if (transaction == null)
             {
