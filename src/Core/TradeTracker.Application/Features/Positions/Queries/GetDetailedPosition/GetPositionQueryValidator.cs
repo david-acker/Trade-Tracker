0c4ffe080c40388dffd0cb3770b3559fafc04c96
DavidDAcker <davidacker3@gmail.com>
2021-03-30 22:31:11 -0400

    Create validator

---
+++ src/Core/TradeTracker.Application/Features/Positions/Queries/GetDetailedPosition/GetPositionQueryValidator.cs
@@ -0,0 +1,18 @@
+using FluentValidation;
+using TradeTracker.Application.Features.Transactions.Shared.Validators;
+using TradeTracker.Application.Features.Transactions.Validators.Transactions;
+
+namespace TradeTracker.Application.Features.Positions.Queries.GetPosition
+{
+    public class GetDetailedPositionQueryValidator : AbstractValidator<GetDetailedPositionQuery>
+    {
+        public GetDetailedPositionQueryValidator()
+        {
+            RuleFor(t => t.AccessKey)
+                .SetValidator(new AccessKeyValidator());
+
+            RuleFor(t => t.Symbol)
+                .SetValidator(new SymbolValidator());
+        }
+    }
+}
