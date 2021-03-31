8dc89410d09006731ce9fb4b12cc9fe2749f0c69
DavidDAcker <davidacker3@gmail.com>
2021-03-31 15:37:49 -0400

    Fix interface

--- src/Core/TradeTracker.Application/Features/Positions/Queries/GetPositions/GetPositionsQueryHandler.cs
+++ src/Core/TradeTracker.Application/Features/Positions/Queries/GetPositions/GetPositionsQueryHandler.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using TradeTracker.Application.Exceptions;
 using TradeTracker.Application.Interfaces.Infrastructure;
 using TradeTracker.Application.Interfaces.Persistence;
 using TradeTracker.Application.Models.Pagination;
