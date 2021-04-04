111125fc6adfa6776bee60559c503af440c47629
DavidDAcker <davidacker3@gmail.com>
2021-04-04 13:24:13 -0400

    Invert return

--- src/Core/TradeTracker.Application/Common/Behaviors/ETagComparer.cs
+++ src/Core/TradeTracker.Application/Common/Behaviors/ETagComparer.cs
@@ -1,12 +1,14 @@
+using TradeTracker.Application.Common.Helpers;
+
 namespace TradeTracker.Application.Common.Behaviors
 {
     public static class ETagComparer
     {
         public static bool IsConflict(object resource, string providedETag)
         {
-            var resourceETag = ETagGenerator.Generate(resource);
+            string resourceETag = ETagGenerator.Generate(resource);
 
-            return resourceETag.Equals(providedETag);
+            return !resourceETag.Equals(providedETag);
         }
     }
 }
