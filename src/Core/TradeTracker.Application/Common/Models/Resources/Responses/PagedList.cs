c091f10449c427a094b267824ec7f9369e10dd86
DavidDAcker <davidacker3@gmail.com>
2021-04-07 09:11:33 -0400

    Fix validation, refactor

--- src/Core/TradeTracker.Application/Common/Models/Resources/Responses/PagedList.cs
+++ src/Core/TradeTracker.Application/Common/Models/Resources/Responses/PagedList.cs
@@ -8,6 +8,11 @@ namespace TradeTracker.Application.Common.Models.Resources.Responses
 {
     public class PagedList<T> : List<T>
     {
+        private const int defaultPageNumber = 1;
+        private const int defaultPageSize = 25;
+        private const int minPageSize = 1;
+        private const int maxPageSize = 100;
+
         public int CurrentPage { get; private set; }
         public int TotalPages { get; private set; }
         public int PageSize { get; private set; }
@@ -16,21 +21,25 @@ namespace TradeTracker.Application.Common.Models.Resources.Responses
         public bool HasPrevious => (CurrentPage > 1);
         public bool HasNext => (CurrentPage < TotalPages);
 
-        public PagedList(List<T> items, int count, int pageNumber, int pageSize)
+        public PagedList(List<T> items, int totalCount, int pageNumber, int pageSize)
         {
-            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
-            TotalCount = count;
+            TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+            TotalCount = totalCount;
 
             PageSize = pageSize;
-            CurrentPage = (pageNumber <= TotalPages)
-                ? pageNumber
-                : TotalPages;
+            CurrentPage = pageNumber;
             
             AddRange(items);
         }
 
-        public static PagedList<T> Create(List<T> items, int pageNumber, int pageSize)
+        public static PagedList<T> Create(
+            List<T> items, 
+            int pageNumber, 
+            int pageSize)
         {
+            pageNumber = ConfirmPageNumber(pageNumber);
+            pageSize = ConfirmPageSize(pageSize);
+
             var count = items.Count();
 
             var itemsForPage = items
@@ -41,15 +50,51 @@ namespace TradeTracker.Application.Common.Models.Resources.Responses
             return new PagedList<T>(itemsForPage, count, pageNumber, pageSize);
         }
 
-        public async static Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
+        public async static Task<PagedList<T>> CreateAsync(
+            IQueryable<T> source, 
+            int pageNumber, 
+            int pageSize)
         {
+            pageNumber = ConfirmPageNumber(pageNumber);
+            pageSize = ConfirmPageSize(pageSize);
+
             var count = source.Count();
+
             var items = await source
                 .Skip((pageNumber - 1) * pageSize)
                 .Take(pageSize)
                 .ToListAsync();
         
             return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
+
+        private static int ConfirmPageNumber(int pageNumber)
+        {
+            return (pageNumber > 0)
+                ? pageNumber
+                : defaultPageNumber;
+        }
+
+        private static int ConfirmPageSize(int pageSize)
+        {
+            int pageSizeToReturn;
+
+            switch (pageSize)
+            {
+                case int size when size < minPageSize:
+                    pageSizeToReturn = minPageSize;
+                    break;
+                
+                case int size when size > maxPageSize:
+                    pageSizeToReturn = maxPageSize;
+                    break;
+                
+                default:
+                    pageSizeToReturn = pageSize;
+                    break;
+            }
+            
+            return pageSizeToReturn;
         }
     }
 }
