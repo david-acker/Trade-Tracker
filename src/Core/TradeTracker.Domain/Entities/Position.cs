1f8ccb8544d766b2d6f851b22cef60b4a70a6ddd
david-acker <davidacker3@gmail.com>
2021-05-01 13:33:38 -0400

    Move functionality into Position class

--- src/Core/TradeTracker.Domain/Entities/Position.cs
+++ src/Core/TradeTracker.Domain/Entities/Position.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using TradeTracker.Domain.Common;
 using TradeTracker.Domain.Enums;
 using TradeTracker.Domain.Interfaces;
@@ -7,6 +8,10 @@ namespace TradeTracker.Domain.Entities
 {
     public class Position : AuditableEntity, IAuthorizableEntity
     {
+        public string Symbol { get; set; }
+
+        public decimal Quantity { get; set; } = decimal.Zero;
+
         public Position() { }
 
         public Position(Guid accessKey, string symbol)
@@ -15,10 +20,80 @@ namespace TradeTracker.Domain.Entities
             Symbol = symbol;
         }
 
-        public string Symbol { get; set; }
+        public bool IsClosed => Quantity == decimal.Zero;
 
-        public ExposureType Exposure { get; set; } = ExposureType.None;
+        public ExposureType Exposure => GetExposure();
 
-        public decimal Quantity { get; set; } = decimal.Zero; 
+        private ExposureType GetExposure()
+        {
+            switch (Quantity)
+            {
+                case decimal q when q > decimal.Zero:
+                    return ExposureType.Long;
+
+                case decimal q when q < decimal.Zero:
+                    return ExposureType.Short;
+            
+                default:
+                    return ExposureType.None;
+            }
+        }
+
+        public void Attach(TransactionType type, decimal quantity)
+        {
+            Quantity += CreateSignedQuantityDelta(type, quantity);
+        }
+
+        public void Attach(Transaction transaction)
+        {
+            if (transaction.Symbol == Symbol)
+            {
+                Attach(transaction.Type, transaction.Quantity);
+            }
+        }
+
+        public void AttachBatch(IEnumerable<Transaction> transactions)
+        {
+            foreach (var transaction in transactions)
+            {
+                Attach(transaction);
+            }
+        }
+
+        public void Detach(TransactionType type, decimal quantity)
+        {
+            Quantity -= CreateSignedQuantityDelta(type, quantity);
+        }
+
+        public void Detach(Transaction transaction)
+        {
+            if (transaction.Symbol == Symbol)
+            {
+                Detach(transaction.Type, transaction.Quantity);
+            }
+        }
+
+        public void DetachBatch(IEnumerable<Transaction> transactions)
+        {
+            foreach (var transaction in transactions)
+            {
+                Detach(transaction);
+            }
+        }
+
+        private static decimal CreateSignedQuantityDelta(TransactionType type, decimal quantity)
+        {
+            switch (type)
+            {
+                case TransactionType.Buy:
+                    return Math.Abs(quantity);
+
+                case TransactionType.Sell:
+                    return -Math.Abs(quantity);
+
+                default:
+                    return decimal.Zero;
+            }
+        }
     }
 }
